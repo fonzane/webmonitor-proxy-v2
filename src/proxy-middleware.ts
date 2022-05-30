57065c02dd6f0255f131ed71be7b1536e065c249
fonzane <timschaffert@yahoo.de>
2022-05-30 13:26:56 +0200

    debugging headers

--- src/proxy-middleware.ts
+++ src/proxy-middleware.ts
@@ -36,6 +36,7 @@ export class ProxyMiddleware implements NestMiddleware {
 
   public use(req: Request, res: Response, next: (error?: any) => void) {
     console.log('cookies ', req.cookies);
+    console.log('headers ', req.headers);
     let target;
     if (req.query.target) {
       target = req.query.target
