39138f5b763a6cd5823d967730111f028e07ca21
fonzane <timschaffert@yahoo.de>
2022-09-22 16:24:25 +0200

    commit

--- src/jwt-auth-middleware.ts
+++ src/jwt-auth-middleware.ts
@@ -6,8 +6,6 @@ import { jwtConstants } from "./jwt-secret";
 export const authenticateJWT = (req: Request, res: Response, next) => {
     const authCookie = req.cookies.authorization;
 
-    console.log('authCookie: ', authCookie);
-
     if (authCookie) {
         const token = authCookie;
         jwt.verify(token, jwtConstants.secret, (err, user) => {
--- src/proxy-middleware.ts
+++ src/proxy-middleware.ts
@@ -3,38 +3,6 @@ import { ClientRequest } from "http";
 import { Request, Response } from 'express';
 import { createProxyMiddleware } from "http-proxy-middleware";
 
-export class ProxyMiddleware implements NestMiddleware {
-  private proxy = createProxyMiddleware({
-    target: 'http://localhost:3000',
-    changeOrigin: true,
-    router: this.customRouter,
-    logger: console,
-    pathRewrite: this.onRewritePath,
-    ws: true,
-  })
-
-  customRouter(req: Request) {
-    let target = req.query.target;
-    console.log('target in router: ', target);
-    let cookieTarget = req.cookies.target;
-    console.log('cookieTarget in router: ', cookieTarget);
-    if (target) {
-      return `http://${target}`;
-    } else if (cookieTarget) {
-      return `http://${cookieTarget}`;
-    } 
-  }
-
-  onRewritePath(string: string):string {
-    if (string.includes("?target")) return string.split("?")[0];
-    else return string;
-  }
-
-  public use(req: Request, res: Response, next: (error?: any) => void) {
-    this.proxy(req, res, next);
-  }
-}
-
 export function proxyMiddleware2(req: Request, res: Response, next) {
 
   let target;
