0ac9630b48b191a24660d40df454673374ccebe2
fonzane <timschaffert@yahoo.de>
2023-09-21 11:21:15 +0200

    added permission check for the resource for users and admins

--- src/jwt-auth-middleware.ts
+++ src/jwt-auth-middleware.ts
@@ -13,6 +13,19 @@ export const authenticateJWT = (req: Request, res: Response, next) => {
                 throw new UnauthorizedException();
             }
             req['user'] = user;
+
+            let target = req.query.target as string;
+            if (target) {
+                if ((user.roles.includes("admin") || user.roles.includes("user")) && (!user.allowedIPs.length || !(user.allowedIPs as string[]).some(ip => target.includes(ip)))) {
+                    throw new UnauthorizedException({
+                        statusCode: 401,
+                        message: "Unauthorized",
+                        reason: "The User doesn't have permission to access the requested IP-Adress.",
+                        solution: "Ask a privileged administrator for permission to access the requested resource."
+                    });
+                }
+            }
+
             next();
         })
     } else {
