ac599392987b8b97c2b3c5b37474083897cd0887
fonzane <timschaffert@yahoo.de>
2023-11-08 17:45:06 +0100

    added safety for maui proxying

--- src/jwt-auth-middleware.ts
+++ src/jwt-auth-middleware.ts
@@ -15,8 +15,20 @@ export const authenticateJWT = (req: Request, res: Response, next) => {
             req['user'] = user;
 
             let target = req.query.target as string;
+            let maui = req.query.maui as string;
             if (target) {
                 if ((user.roles.includes("admin") || user.roles.includes("user")) && (!user.allowedIPs.length || !(user.allowedIPs as string[]).some(ip => target.includes(ip)))) {
+                    throw new UnauthorizedException({
+                        statusCode: 401,
+                        message: "Unauthorized",
+                        reason: "The User doesn't have permission to access the requested IP-Adress.",
+                        solution: "Ask a privileged administrator for permission to access the requested resource."
+                    });
+                }
+            }
+
+            if (maui) {
+                if ((user.roles.includes("admin") || user.roles.includes("user")) && (!user.allowedIPs.length || !(user.allowedIPs as string[]).some(ip => maui.includes(ip)))) {
                     throw new UnauthorizedException({
                         statusCode: 401,
                         message: "Unauthorized",
